3418dca164f0c2c0a420945b6c3a76fce2a85643
Trung Nguyen <nctrung04102000@gmail.com>
2021-11-19 22:18:50 +0700

    Update . . .

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Trung Nguyen</p>
+        <i>“𝘌𝘷𝘦𝘳𝘺𝘵𝘩𝘪𝘯𝘨 𝘸𝘪𝘭𝘭 𝘣𝘦 𝘦𝘢𝘴𝘺 𝘪𝘧 𝘺𝘰𝘶 𝘵𝘳𝘺 𝘩𝘢𝘳𝘥...”</i>
       </header>
     </div>
   );
